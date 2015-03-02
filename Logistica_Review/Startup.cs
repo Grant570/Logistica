3e8a34ddb6fefdba600c8c286b4032ccbe2fdfe7
Grant570 <gs339@nau.edu>
2015-03-01 21:32:34 -0700

    testing to see if this works

---
+++ Logistica_Review/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Logistica_Review.Startup))]
+namespace Logistica_Review
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
