704ff9563cd45df2b3a57aa6dc53ef51248e2c5a
Grant570 <gs339@nau.edu>
2015-04-08 22:51:04 -0700

    Added a unit test where other tests can be added

---
+++ Logistica_Review_Tests/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logistica_Review;
+using Logistica_Review.Controllers;
+using Logistica_Review.Models;
+using System.Web.Mvc;
+using System.Text;
+using System.Collections.Generic;
+
+//https://www.visualstudio.com/get-started/code/create-and-run-unit-tests-vs
+namespace Logistica_Review_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HomeController controller = new HomeController();
+
+            ViewResult result = controller.Index() as ViewResult;
+
+            //this needs work
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
